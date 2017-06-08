8d1011a3b411ecfde4ee2e6b3ccb3fad4a7c74fb
Steffen <steffen_son@hotmail.com>
2017-06-08 13:53:23 +0200

    opdateret til runnable

--- ChatPlatform/src/accessServerSide/Server.java
+++ ChatPlatform/src/accessServerSide/Server.java
@@ -3,24 +3,28 @@ package accessServerSide;
 import java.io.*;
 import java.net.*;
 
-public class Server {
-	
-	public void run() throws Exception{
-	ServerSocket serverSocket = new ServerSocket(666);
-	Socket socket = serverSocket.accept();
-	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-    BufferedReader in = new BufferedReader(
-            new InputStreamReader(socket.getInputStream()));
-    
-    
-    String message = in.readLine();
-    System.out.println(message);
-    
-    if(message != null){
-    	PrintStream printStream = new PrintStream(socket.getOutputStream());
-    	printStream.println("serverside --> Clintside");
-    }
-	
+public class Server implements Runnable {
+
+	public void run() {
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(666);
+
+			Socket socket = serverSocket.accept();
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String message = in.readLine();
+			System.out.println(message);
+
+			if (message != null) {
+				PrintStream printStream = new PrintStream(socket.getOutputStream());
+				printStream.println("serverside --> Clintside");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
