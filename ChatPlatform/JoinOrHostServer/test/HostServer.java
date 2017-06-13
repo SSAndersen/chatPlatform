f19519046879fef79b3778e57463454e766538a7
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-06-13 11:54:50 +0200

    added port TextField

--- ChatPlatform/JoinOrHostServer/test/HostServer.java
+++ ChatPlatform/JoinOrHostServer/test/HostServer.java
@@ -5,14 +5,17 @@ import java.net.UnknownHostException;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class HostServer {
-
+	TextField portText;
+	
 	public GridPane createGrid() throws UnknownHostException {
 		GridPane grid = new GridPane();
 		grid.add(new Label("Your ipadress: " + InetAddress.getLocalHost().getHostAddress()), 0, 0);
-		grid.add(new Label("Your port: "), 0, 1);
+		grid.add(portHboxCreator("Choose port: ", portText = new TextField()), 0, 1);
 		
 		grid.add(hostButton(), 0, 2);
 
@@ -27,5 +30,14 @@ public class HostServer {
 		});
 		
 		return hostButton;
+	}
+	
+	private HBox portHboxCreator(String labelText, TextField textField) {
+		HBox writeHBox = new HBox();
+		Label label = new Label(labelText);
+
+		writeHBox.getChildren().addAll(label, textField);
+
+		return writeHBox;
 	}
 }
